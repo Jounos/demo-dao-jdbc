564a1aa3ad4f032d2ff4ce1efec65f59d044d68a
Jounos <gnliberato@outlook.com>
2019-01-04 18:11:10 -0200

    Implementação do findById

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,21 +1,13 @@
 package Application;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
 
 import model.dao.DaoFactory;
-import model.entities.Department;
-import model.entities.Seller;
+import model.dao.SellerDao;
 
 public class Program {
 	public static void main(String[] args) throws ParseException {
-		Locale.setDefault(Locale.US);
-		Department obj=new Department(1, "Books");
-		SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
-		Seller sel=new Seller(1, "Alex Green", "alex@gmail.com", sdf.parse("16/05/1991"), 10000.00, obj);
-		System.out.println(sel.toString());
-		
 		SellerDao sellerDAO=DaoFactory.createSellerDao();
+		System.out.println(sellerDAO.findById(3));
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
 
+	private Connection con;
+	
+	public SellerDaoJDBC(Connection con) {
+		this.con=con;
+	}
+	
 	@Override
 	public void inserir(Seller obj) {
 		// TODO Auto-generated method stub
@@ -27,14 +40,48 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement stm=null;
+		ResultSet rs=null;
+		try {
+			StringBuilder sql=new StringBuilder();
+			sql.append("SELECT ");
+			sql.append("s.*, d.desc_name as depName ");
+			sql.append("FROM ");
+			sql.append("seller s ");
+			sql.append("INNER JOIN ");
+			sql.append("department d ON d.id=s.departmentid ");
+			sql.append("WHERE ");
+			sql.append("s.id = ?");
+			stm=con.prepareStatement(sql.toString());
+			stm.setInt(1, id);
+			rs=stm.executeQuery();
+			if(rs.next()) {
+				Department dep=new Department();
+				dep.setId(rs.getInt("departmentid"));
+				dep.setName(rs.getString("depName"));
+				Seller sel=new Seller();
+				sel.setId(rs.getInt("id"));
+				sel.setName(rs.getString("desc_name"));
+				sel.setEmail(rs.getString("email"));
+				sel.setBirthDate(rs.getDate("birthdate"));
+				sel.setBaseSalary(rs.getDouble("basesalary"));
+				
+				sel.setDepartment(dep);
+				return sel;
+			}
+			return null;
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(stm);
+			DB.closeResutSet(rs);
+			DB.closeConnection();
+		}
 	}
-
+	
 	@Override
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return null; 	
 	}
-
 }
